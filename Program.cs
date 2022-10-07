82a0d4a1daebbe137defd813196956813285d113
Shinawonk16 <shinawonk@gmail.com>
2022-10-07 07:52:13 -0700

    Array question

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace ARRAY_002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+                //SORTING AND SEARCHING...
+            
+         /*int[] myArray = {13,27,41,67,99,83};
+         int num = 83;
+         bool isFound =true;
+         for (int i = 0; i <myArray.Length; i++)
+         {
+            
+            if (num == myArray[i])
+            {
+                isFound = true;
+             System.Console.WriteLine(i); 
+             break;
+             
+               
+            }
+            else 
+            {
+               isFound = false;
+            }
+         }
+         if (isFound == false )
+         {
+            System.Console.WriteLine("not found");
+         }*/
+
+
+        /* int[] myArray = {1,2,4,4,4,4,3,3,2,2,2,2,5,5,5,5,5,5,7,9};
+         int element = 0, count = 0, countholder = 0, elementholder = 0;
+         for (int i = 0; i <myArray.Length -1; i++)
+         {
+            if (myArray[i] == myArray[i+1])
+            {
+               element =myArray[i] ;
+               count++;
+            }
+            else
+            {
+                if (count > countholder )
+                {
+                    countholder = count;
+                    count = 0;
+                    elementholder = element;   
+                }
+            }
+         }
+         for (int j = 1; j <= countholder+1; j++)
+         {
+          System.Console.Write(countholder + "," );  
+         }
+         System.Console.WriteLine();*/
+
+         int[] myArray= {2,7,9,6,8,10,11,12,14,8};
+         int difference = 0, occurence = 0,
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
